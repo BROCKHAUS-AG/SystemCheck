536d035130d76f306613dad1e28dcbeccb6558e5
Paul Mizel <pmizel@users.noreply.github.com>
2017-05-31 15:09:06 +0200

    Add files via upload

--- checkRegedit.ps1
+++ checkRegedit.ps1
@@ -42,6 +42,7 @@ function BagCheckRegedit-DisableAttachSecurityWarning{
     param([bool]$Debug=$false,
 	      [bool]$Fix=$false,		  
 		  [String]$Text="DisableAttachSecurityWarning",
+		  [String]$Name="DisableAttachSecurityWarning",
 		  [object]$Path="Registry::HKEY_CURRENT_USER\Software\Microsoft\VisualStudio\14.0\Debugger",
 		  [object]$Compare=1
 		  )
@@ -50,15 +51,15 @@ function BagCheckRegedit-DisableAttachSecurityWarning{
 	
 	Try{
 	
+		#Get-ItemProperty -Path "Registry::HKEY_CURRENT_USER\Software\Microsoft\VisualStudio\14.0\Debugger" -Name "DisableAttachSecurityWarning"
 	    $result = Get-ItemProperty -Path $Path -Name $Name
 	    		
 		if($Debug)
 		{
-			#write-host $result		
-			#write-host "RES:" $result.DisableAttachSecurityWarning
+			write-host $result
 		}
 		
-		If ($result.DisableAttachSecurityWarning -eq $Compare)
+		If ($result.$Name -eq $Compare)
 		{		
 			write-host ("OK ( Regedit "+ $Text +" : 1)") -ForegroundColor Green
 		}
@@ -80,4 +81,50 @@ function BagCheckRegedit-DisableAttachSecurityWarning{
 			write-host "Ex:F:Regedit, M:" $_.Exception.Message ")" -ForegroundColor Red
 		}
 	}
-} 
+}
+
+function BagCheckRegedit-MSDTC{
+    
+    param([bool]$Debug=$false,
+	      [bool]$Fix=$false,		  
+		  [String]$Text="NetworkDtcAccess",
+		  [String]$Name="NetworkDtcAccess",
+		  [object]$Path="Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\MSDTC\Security",
+		  [object]$Compare=1
+		  )
+	
+	if(-NOT $Debug) { $ErrorActionPreference = "Stop" }
+	
+	Try{
+	
+		#Get-ItemProperty -Path "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\MSDTC\Security" -Name "NetworkDtcAccess"
+	    $result = Get-ItemProperty -Path $Path -Name $Name
+	    		
+		if($Debug)
+		{
+			write-host $result
+		}
+		
+		If ($result.$Name -eq $Compare)
+		{		
+			write-host ("OK ( Regedit "+ $Text +" : 1)") -ForegroundColor Green
+		}
+		Else
+		{
+			write-host "ERROR: ("  $result ") ." -ForegroundColor Red
+			If($Fix)
+			{
+				write-host "FIX: ("  $status ") Enable MSDTC Settings" -ForegroundColor Yellow								
+				#[Microsoft.Win32.Registry]::SetValue($Path,"DisableAttachSecurityWarning",1,[Microsoft.Win32.RegistryValueKind]::DWord)
+				regedit /s MSDTC.Security.reg
+			}
+		}
+	}
+	Catch
+	{
+		if(-NOT $Debug)
+		{
+			write-host "Ex:F:Regedit, M:" $_.Exception.Message ")" -ForegroundColor Red
+		}
+	}
+}
